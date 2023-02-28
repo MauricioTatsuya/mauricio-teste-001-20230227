c802038c1c090c1121e0a0ae7f1d451ff95cc5fe
Maurício Tatsuya Fujino <126506090+MauricioTatsuya@users.noreply.github.com>
2023-02-28 20:31:47 -0300

    Add files via upload

---
+++ Codigo-exercicio05-teste-20230227-versao03.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+/*5) Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+	a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+	b) Evite usar funções prontas, como, por exemplo, reverse;*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+std::string entrada ="Ola Mundo!";
+std::string saida = entrada; 
+int i, j, tamanho;
+tamanho = entrada.size();
+for(i=0, j=tamanho-1;i<tamanho-1,j>=0;i++,j--)
+   { saida[j]=entrada[i];}
+cout<<"String original: \n";
+cout<<entrada;
+cout<<"\n";
+cout<<"String invertida: \n";
+cout<<saida;
+cout<<"\n";
+return 0;
+}
+
